7e1e31a6a13f477635c3e1aa8b7f1062622c3b2d
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2020-03-24 15:19:24 +0100

    Started with Tutorial 03.

---
+++ code/Tutorial_03/TestDialog.cpp
@@ -0,0 +1,51 @@
+#include "TestDialog.h"
+
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+
+#include "RectangleWindow.h"
+
+TestDialog::TestDialog() {
+	// first create the OpenGL window
+	QSurfaceFormat format;
+	format.setRenderableType(QSurfaceFormat::OpenGL);
+	format.setProfile(QSurfaceFormat::CoreProfile);
+	format.setVersion(3,3);
+
+	m_rectangleWindow = new RectangleWindow;
+	m_rectangleWindow->setFormat(format);
+
+	// we now create a widget container
+	QWidget *container = QWidget::createWindowContainer(m_rectangleWindow);
+	container->setMinimumSize(QSize(640,480));
+	container->setFocusPolicy(Qt::TabFocus);
+
+	// create the layout and insert widget container
+	QVBoxLayout * vlay = new QVBoxLayout;
+	vlay->addWidget(container);
+
+	// now create some buttons at the bottom
+
+	QHBoxLayout * hlay = new QHBoxLayout;
+
+	QPushButton * changeColorBtn = new QPushButton(tr("Change colors"), this);
+	connect(changeColorBtn, &QPushButton::clicked, this, &TestDialog::onChangeColorsClicked);
+
+	QPushButton * closeBtn = new QPushButton(tr("Close"), this);
+	connect(closeBtn, &QPushButton::clicked, this, &QDialog::accept);
+
+	hlay->addWidget(changeColorBtn);
+	hlay->addStretch();
+	hlay->addWidget(closeBtn);
+
+	vlay->addLayout(hlay);
+
+	setLayout(vlay);
+}
+
+
+void TestDialog::onChangeColorsClicked() {
+	// randomize the colors and change them in the OpenGL window
+
+}
---
+++ code/Tutorial_03/TestDialog.h
@@ -0,0 +1,21 @@
+#ifndef TESTDIALOG_H
+#define TESTDIALOG_H
+
+#include <QDialog>
+
+class RectangleWindow;
+
+/*! A simple test dialog for an embedded OpenGL window. */
+class TestDialog : public QDialog {
+	Q_OBJECT
+public:
+	TestDialog();
+
+private slots:
+	void onChangeColorsClicked();
+
+private:
+	RectangleWindow * m_rectangleWindow;
+};
+
+#endif // TESTDIALOG_H
