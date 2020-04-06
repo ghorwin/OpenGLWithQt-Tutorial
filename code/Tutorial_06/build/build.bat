bde6f3a75ad030ecc35ee8d55c253e63d374fc28
Andreas Nicolai <andreas.nicolai@gmx.net>
2020-04-06 08:56:02 +0200

    More work on CMake chapter.

--- code/Tutorial_06/build/build.bat
+++ code/Tutorial_06/build/build.bat
@@ -6,8 +6,12 @@ call "c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 :: path to Qt
 set CMAKE_PREFIX_PATH=c:\Qt\5.11.3\msvc2015_64
 
+:: we build in subdir
+mkdir vc_x64_release
+cd vc_x64_release
+
 :: configure in release mode, to build with JOM
-cmake -G "NMake Makefiles JOM" -DCMAKE_BUILD_TYPE:String="Release" ..  
+cmake -G "NMake Makefiles JOM" -DCMAKE_BUILD_TYPE:String="Release" ..\..  
 
 :: build
 jom
--- code/Tutorial_06/build/deploy.bat
+++ code/Tutorial_06/build/deploy.bat
@@ -4,8 +4,10 @@
 set CMAKE_PREFIX_PATH=c:\Qt\5.11.3\msvc2015_64
 
 :: deployment
+mkdir vc_x64_deploy
+copy vc_x64_release\Tutorial_06.exe vc_x64_deploy
 
 :: copy dependencies
-%CMAKE_PREFIX_PATH%\bin\windeployqt Tutorial_06.exe
+%CMAKE_PREFIX_PATH%\bin\windeployqt vc_x64_deploy\Tutorial_06.exe
 
 
